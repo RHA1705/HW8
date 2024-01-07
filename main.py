3681d64b332835c779a2ecf0bbceb2f51c723d44
RHA1705 <garbarzroman47@gmail.com>
2024-01-07 12:26:44 +0100

    last

--- main.py
+++ main.py
@@ -1,52 +1,48 @@
-from datetime import date, datetime
+'''FIle for remember users birthdays'''
+from datetime import date, datetime, timedelta
 
 
 def get_birthdays_per_week(users):
+    '''Function det dict with name and date birthday users and return dict with usernames'''
     # Реалізуйте тут домашнє завдання
     week_birthdays = {}
     for user in users:
         user_name = user['name'].split()[0]
         birthday = user['birthday']
-        weekday_birth = birthday.strftime('%A')
-        # print(user_name, weekday_birth)
         today = date.today()
         sunday = 6 - today.weekday()
         date_sunday = today.replace(day=today.day + sunday)
-        print(date_sunday)
         days = []
-        names = []
+        user_names = []
         if birthday < today:
+            birthday = birthday.replace(year=today.year + 1)
+        weekday_birth = birthday.strftime('%A')
+        if not (birthday > today and birthday < today + timedelta(days=7)):
             continue
         for day in range(today.weekday(), date_sunday.weekday() + 1):
-            day = today.replace(day = day + 1).strftime('%A')
-            # print(date_sunday.weekday())
+            day = (today + timedelta(days=day)).strftime('%A')
             days.append(day)
-            print(day)
             if weekday_birth == day:
                 if day == 'Saturday' or day == 'Sunday':
                     if 'Monday' in week_birthdays:
                         week_birthdays.get('Monday').append(user_name)
                     else:
-                        names.append(user_name)
-                        week_birthdays.update({'Monday' : names})
+                        user_names.append(user_name)
+                        week_birthdays.update({'Monday' : user_names})
                 elif day not in week_birthdays:
-                    names.append(user_name)  
-                    week_birthdays.update({day : names})  
+                    user_names.append(user_name)
+                    week_birthdays.update({day : user_names})
                 else:
                     week_birthdays.get(day).append(user_name)
-                 
-        # print(days)
-    
-    print(week_birthdays)
     return week_birthdays
 
 
 if __name__ == "__main__":
-    users = [
+    example_users = [
         {"name": "Jan Koum", "birthday": datetime(1976, 1, 1).date()},
     ]
 
-    result = get_birthdays_per_week(users)
+    result = get_birthdays_per_week(example_users)
     print(result)
     # Виводимо результат
     for day_name, names in result.items():
